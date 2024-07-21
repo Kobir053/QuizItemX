8246c62196c8d1c018ee6a366bc89e230ab68fb1
Kobir053 <kobir05333@gmail.com>
2024-07-21 17:03:23 +0300

    Add project files.

---
+++ QuizItemX/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace QuizItemX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. enter question\n2. see questions");
+            int res = Convert.ToInt32(Console.ReadLine());
+            XService service = new XService("./QuizItem.xml");
+            if (res == 1)
+            {
+                Console.WriteLine("enter your question");
+                string q = Console.ReadLine();
+                Console.WriteLine("enter answer");
+                string ans = Console.ReadLine();
+                service.AddItem(q, ans);
+            }
+            else
+            {
+                service.Show();
+            }
+
+        }
+    }
+}
---
+++ QuizItemX/XService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QuizItemX
+{
+    internal class XService
+    {
+        private string _path;
+        public XService(string path) 
+        {
+            _path = path;
+        }
+
+        public void AddItem(string q, string ans)
+        {
+            XDocument doc = XDocument.Load(_path);
+            XElement root = doc.Root;
+
+            XElement qNode = new XElement("Question",q);
+            XElement aNode = new XElement("Answer",ans);
+            XElement item = new XElement("Item",qNode, aNode);
+            root.Add(item);
+            doc.Save(_path);
+        }
+
+        public void Show()
+        {
+            XDocument doc = XDocument.Load(_path);
+            XElement root = doc.Root;
+
+            List<XElement> list = root.Elements().ToList();
+            foreach (XElement node in list)
+            {
+                string a = node.Element("Question").Value;
+            }
+        }
+    }
+}
